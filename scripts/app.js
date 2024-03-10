667a1f68eab5d7bfecba844e0894d5a0f798e29f
Preetha Pinto <preethacp@gmail.com>
2024-03-10 18:38:41 +1000

    Add light mode and sticky navbar

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,48 @@
 const navLinkList = document.querySelectorAll('.nav-link');
 
 navLinkList.forEach(link => {
-    link.addEventListener('click', () => {
-        document.querySelector('.active')?.classList.remove('active')
+    link.addEventListener('click', (event) => {
+        document.querySelector('.active')?.classList.remove('active');
+        
         link.classList.add('active');
+        
+        if (link.getAttribute('href') === '#portfolio') {
+            event.preventDefault();
+            
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth' 
+            });
+        }
     });
 });
+
+document.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+})
+
+let btn = document.getElementById('btn'); 
+let logo = document.querySelector('.logo img')
+
+btn.onclick = function() {
+    document.body.classList.toggle('light');
+
+    let darkLogo = logo.getAttribute('data-dark');
+    let lightLogo = logo.getAttribute('data-light');
+
+    let currentLogo = logo.getAttribute('src');
+    
+    if (currentLogo === darkLogo) {
+        logo.setAttribute('src', lightLogo);
+    } else {
+        logo.setAttribute('src', darkLogo)
+    }
+}
+
+
